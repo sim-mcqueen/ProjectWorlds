927580f405b9ef1b2e2bd1486e0072072526e587
andrew-dahlmanoeth <andrew.dahlmanoeth@digipen.edu>
2021-11-02 13:04:57 -0700

    EnemyShoot.cs Updated
    Now works as intended rather than speed based on slope

--- Assets/Scripts/EnemyShoot.cs
+++ Assets/Scripts/EnemyShoot.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class EnemyShoot : MonoBehaviour
 {
+    public float projSpeed = 200f;
     private int cooldown = 3;
-    private Vector2 projVel;
+    private Vector2 projDir;
     [SerializeField]
     private GameObject fireTo;
     [SerializeField]
@@ -13,16 +14,19 @@ public class EnemyShoot : MonoBehaviour
 
     private void Start()
     {
+        Debug.Log("Start");
         StartCoroutine(ShootAt());
     }
 
     IEnumerator ShootAt()
     {
-        projVel = fireTo.transform.position - transform.position;
+        Debug.Log("Shoot");
+        projDir = fireTo.transform.position - transform.position;
         GameObject proj = Instantiate(projectile, transform.position, Quaternion.identity);
         Rigidbody2D rb = proj.GetComponent<Rigidbody2D>();
-        rb.velocity += projVel;
+        proj.transform.right = fireTo.transform.position - transform.position;
+        rb.AddRelativeForce(proj.transform.right * projSpeed);
         yield return new WaitForSeconds(cooldown);
-        ShootAt();
+        StartCoroutine(ShootAt());
     }
 }
