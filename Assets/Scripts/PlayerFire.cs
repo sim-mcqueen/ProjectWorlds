2ef7ae6280f92a7f92ea470fb6754d61e4798d5f
sim-mcqueen <s.mcqueen@digipen.edu>
2021-11-02 13:12:16 -0700

    playershoot

--- Assets/Scripts/PlayerFire.cs
+++ Assets/Scripts/PlayerFire.cs
@@ -6,7 +6,7 @@ public class PlayerFire : MonoBehaviour
 {
     public GameObject bullet;
     public float bulletSpeed;
-
+    public float projSpeed = 500f;
 
 
     private void Update()
@@ -16,9 +16,10 @@ public class PlayerFire : MonoBehaviour
             Vector3 mouse = Input.mousePosition;
             Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(new Vector3(mouse.x, mouse.y, transform.position.y));
             Vector2 forward = mouseWorld - transform.position;
-            bullet = Instantiate(bullet, transform.position, Quaternion.identity);
-            Rigidbody2D RB = bullet.GetComponent<Rigidbody2D>();
-            RB.velocity += forward;
+            GameObject proj = Instantiate(bullet, transform.position, Quaternion.identity);
+            Rigidbody2D rb = proj.GetComponent<Rigidbody2D>();
+            proj.transform.right = forward;
+            rb.AddRelativeForce(proj.transform.right * projSpeed);
         }
     }
 
